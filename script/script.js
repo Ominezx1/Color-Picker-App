ebabd34b0160731ef058aa78064daccef3d94f0e
Ominezx1 <Ominezx9@gmail.com>
2022-09-04 21:43:22 +0100

    First Push to Git

---
+++ script/script.js
@@ -0,0 +1,32 @@
+//fetching colors
+
+///---- This sucked in local dev... 
+// fetch('https://colornames.org/fresh/json/', {mode: 'no-cors'})
+// .then(function (response){
+//     if(response.ok()){
+//         return response.json();
+//     }else{
+//         return Promise.reject(response);
+//     }
+// })
+// .then(function(data){
+//     console.log(data);
+// })
+// .catch(function(err){
+//     console.warn("something went wrong", err);
+
+// })
+
+//Goooouuu jouub
+///---But then dumpster fire
+// fetch('https://colornames.org/fresh/json/', {mode: 'no-cors'}).then
+// (function(response){
+//     //Denote call being successful
+//     console.log('success', response);
+// }).then(function(data){
+//     console.log(data);
+// }).catch(function(err){
+//     //Denote error
+//     console.warn('something went wrong', err);
+// })
+
